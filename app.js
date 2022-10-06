88761d9dd1d8d0988ec4f5cf5daa490f43c875c8
Michal Grabara <michalgrabara84@gmail.com>
2022-10-06 23:07:06 +0200

    save file

--- app.js
+++ app.js
@@ -3,64 +3,102 @@ const content = document.querySelector(".content");
 
 const fontSizeUp = document.querySelector('.fontsizeup');
 const fontSizeDown = document.querySelector('.fontsizedown');
-
 const fontsizespan = document.querySelector('.fontsizespan');
 
-// editor
+const loadBtn = document.getElementById('loadbtn');
+const saveBtn = document.getElementById('Savebtn');
+const saveAsBtn = document.getElementById('SaveAsbtn');
 
-for (let i = 0; i < buttons.length; i++) {
-    buttons[i].addEventListener('click', (e) => {
-        let command = e.target.getAttribute('data');
+const main = () => {
+    //editor
 
-        if (command === "createLink" || command === "InsertImage") {
-            let url = prompt("Link: ");
-            document.execCommand(command, false, url)
-        } else {
-            document.execCommand(command, false, null);
-        }
+    for (let i = 0; i < buttons.length; i++) {
+        buttons[i].addEventListener('click', (e) => {
+            let command = e.target.getAttribute('data');
+    
+            if (command === "createLink" || command === "InsertImage") {
+                let url = prompt("Link: ");
+                document.execCommand(command, false, url)
+            } else {
+                document.execCommand(command, false, null);
+            }
+        })
+    }
+    
+    let font = "";
+    let fontsize = 18;
+    
+    function updateFont() {
+        let selectFont = document.querySelector('#select-font');
+        font = selectFont.options[selectFont.selectedIndex].value;
+    
+        content.style = 'font-family:' + font + ";";
+    }
+    
+    updateFont();
+    
+    function updateColorFont() {
+        let selectColorFont = document.querySelector('#color-font');
+        color = selectColorFont.options[selectColorFont.selectedIndex].value;
+    
+        content.style = 'color:' + color + ";";
+    }
+    
+    updateColorFont();
+    
+    function updateFontsSizeSpan() {
+        fontsizespan.innerHTML = "";
+        fontsizespan.innerHTML = fontsize + "px";
+    }
+    
+    updateFontsSizeSpan();
+    
+    fontSizeUp.addEventListener('click', () => {
+        fontsize++;
+        content.style = "font-size:" + fontsize + "px;";
+        updateFontsSizeSpan();
     })
-}
+    
+    fontSizeDown.addEventListener('click', () => {
+        fontsize--;
+        content.style = "font-size:" + fontsize + "px;";
+        updateFontsSizeSpan();
+    })
 
-let font = "";
-let fontsize = 18;
 
-function updateFont() {
-    let selectFont = document.querySelector('#select-font');
-    font = selectFont.options[selectFont.selectedIndex].value;
+    //save btn
+    let fileHandle;
 
-    content.style = 'font-family:' + font + ";";
-}
+    async function button() {
+        [fileHandle] = await window.showOpenFilePicker();
+        let fileData = await fileHandle.getFile();
+        let text = await fileData.text();
+        textarea.innerHTML = text;
+    }
 
-updateFont();
+    async function save() {
+        let stream = await fileHandle.createWritable();
+        await stream.write(textarea.innerHTML);
+        await stream.close();
+    }
 
-function updateColorFont() {
-    let selectColorFont = document.querySelector('#color-font');
-    color = selectColorFont.options[selectColorFont.selectedIndex].value;
+    async function saveAs() {
+        fileHandle = await window.showSaveFilePicker();
+        save();
+    }
 
-    content.style = 'color:' + color + ";";
-}
+    loadBtn.addEventListener('click', () => {
+        button();
+    })
 
-updateColorFont();
+    saveBtn.addEventListener('click', () => {
+        save();
+    })
 
-function updateFontsSizeSpan() {
-    fontsizespan.innerHTML = "";
-    fontsizespan.innerHTML = fontsize + "px";
-}
+    saveAsBtn.addEventListener('click', () => {
+        saveAs();
+    })
 
-updateFontsSizeSpan();
+};
 
-fontSizeUp.addEventListener('click', () => {
-    fontsize++;
-    content.style = "font-size:" + fontsize + "px;";
-    updateFontsSizeSpan();
-})
-
-fontSizeDown.addEventListener('click', () => {
-    fontsize--;
-    content.style = "font-size:" + fontsize + "px;";
-    updateFontsSizeSpan();
-})
-
-//download
-
-//save on account
+document.addEventListener('DOMContentLoaded', main)
